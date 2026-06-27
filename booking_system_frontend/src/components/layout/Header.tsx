18ccc29945963d62f3069a5feb71c85ad84497df
pushpalatha.b@ibm.com <pushpalathab@Pushpalathas-MacBook-Pro-2.local>
2026-06-27 20:46:51 +0530

    Updated Header file with latest name

--- booking_system_frontend/src/components/layout/Header.tsx
+++ booking_system_frontend/src/components/layout/Header.tsx
@@ -23,7 +23,7 @@ export const Header = () => {
               <Rocket className="text-cosmic-purple" size={32} />
             </motion.div>
             <span className="text-2xl font-bold bg-cosmic-gradient bg-clip-text text-transparent">
-              Galaxium Travel Agency one
+              Galaxium Travel Agency Two
             </span>
           </Link>
 
